42bc25ba6fdf3af0599c208f70d89650f96fd12a
John Krzemien <johnkrz@microsoft.com>
2017-03-04 11:57:25 -0800

    Initial commit with 1 interview question and test setup

---
+++ Interview/Random/Code/AtLeastOneDuplicate.cs
@@ -0,0 +1,43 @@
+﻿namespace Random.Code
+{
+    using System.Collections.Generic;
+
+    public static class AtLeastOneDuplicate
+    {
+        public static int ReturnDuplicateO1Space(List<int> list)
+        {
+            return ReturnDuplicateHelper(list, 1, list.Count);
+        }
+
+        // O(1) Space, O(n log (n)) Time
+        private static int ReturnDuplicateHelper(List<int> list, int startRange, int endRange)
+        {
+            var n = list.Count - 1;
+            var rangeSize = endRange - startRange;
+            if (rangeSize <= 1)
+            {
+                var result = startRange;
+                return result;
+            }
+
+            var count = 0;
+            var middleValue = (endRange + startRange) / 2;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] < middleValue &&
+                    list[i] >= startRange &&
+                    list[i] < endRange)
+                {
+                    count++;
+                }
+            }
+
+            if (count > (rangeSize / 2))
+            {
+                return ReturnDuplicateHelper(list, startRange, middleValue);
+            }
+
+            return ReturnDuplicateHelper(list, middleValue, endRange);
+        }
+    }
+}
---
+++ Interview/Tests/AtLeastOneDuplicateTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Random.Code;
+
+    [TestClass]
+    public class AtLeastOneDuplicateTests
+    {
+        [TestMethod]
+        public void O1Space_Returns1()
+        {
+            const int expected = 1;
+            var actual = AtLeastOneDuplicate.ReturnDuplicateO1Space(new List<int> { 1, 1, 2, 2, 3 }); 
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
