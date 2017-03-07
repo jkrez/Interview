8c0c9b840db790be0fac214b966386d0a68b0e10
John Krzemien <John Krzemien>
2017-03-06 21:06:46 -0800

    Question 1.1 from craking the coding interview

---
+++ Interview/Random/cracking-the-coding-interview/Chapter 1/Question_1_1.cs
@@ -0,0 +1,61 @@
+﻿namespace Random.Cracking_the_Code.Chapter_1
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Implement an algorithm to determine if a string has all unique
+    /// characters. What if you cannot use additional data structures?
+    /// </summary>
+    public class Question_1_1
+    {
+
+        // Time: O(1)
+        // Space: O(N)
+        public static bool AllUniqueCharactersHash(string s)
+        {
+            if (s == null)
+            {
+                return true;
+            }
+
+            HashSet<char> characterSet = new HashSet<char>();
+            foreach (var character in s)
+            {
+                if (characterSet.Contains(character))
+                {
+                    return false;
+                }
+
+                characterSet.Add(character);
+            }
+
+            return true;
+        }
+
+        // Time: (n log(n))
+        // Space: O(1)
+        public static bool AllUniqueCharactersNoAdditionalSpace(string s)
+        {
+            if (s == null)
+            {
+                return true;
+            }
+
+            var array = s.ToCharArray();
+
+            // Array.Sort sorts an array in-place.
+            Array.Sort(array);
+            var orderedString = string.Concat(array);
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (orderedString[i - 1] == orderedString[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Interview/Tests/craking-the-coding-interview/Chapter 1/Test_1_1.cs
@@ -0,0 +1,81 @@
+﻿namespace Tests.Cracking_the_Code.Chapter_1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Random.Cracking_the_Code.Chapter_1;
+
+    [TestClass]
+    public class Test_1_1
+    {
+        [TestMethod]
+        public void Question_1_1_Solution1_ReturnsFalse()
+        {
+            const string s = "test";
+            const bool expected = false;
+
+            Assert.AreEqual(expected, Question_1_1.AllUniqueCharactersHash(s));
+        }
+
+        [TestMethod]
+        public void Question_1_1_Solution2_ReturnsFalse()
+        {
+            const string s = "test";
+            const bool expected = false;
+
+            Assert.AreEqual(expected, Question_1_1.AllUniqueCharactersNoAdditionalSpace(s));
+        }
+
+        [TestMethod]
+        public void Question_1_1_Solution1_ReturnsTrue()
+        {
+            const string s = "tea";
+            const bool expected = true;
+
+            Assert.AreEqual(expected, Question_1_1.AllUniqueCharactersHash(s));
+        }
+
+        [TestMethod]
+        public void Question_1_1_Solution2_ReturnsTrue()
+        {
+            const string s = "tea";
+            const bool expected = true;
+
+            Assert.AreEqual(expected, Question_1_1.AllUniqueCharactersNoAdditionalSpace(s));
+        }
+
+        [TestMethod]
+        public void Question_1_1_Solution1_EmptyString_ReturnsTrue()
+        {
+            const string s = "";
+            const bool expected = true;
+
+            Assert.AreEqual(expected, Question_1_1.AllUniqueCharactersHash(s));
+        }
+
+        [TestMethod]
+        public void Question_1_1_Solution2_EmptyString_ReturnsTrue()
+        {
+            const string s = "";
+            const bool expected = true;
+
+            Assert.AreEqual(expected, Question_1_1.AllUniqueCharactersNoAdditionalSpace(s));
+        }
+
+        [TestMethod]
+        public void Question_1_1_Solution1_NullString_ReturnsTrue()
+        {
+            const string s = null;
+            const bool expected = true;
+
+            Assert.AreEqual(expected, Question_1_1.AllUniqueCharactersHash(s));
+        }
+
+        [TestMethod]
+        public void Question_1_1_Solution2_NullString_ReturnsTrue()
+        { 
+            const string s = null;
+            const bool expected = true;
+
+            Assert.AreEqual(expected, Question_1_1.AllUniqueCharactersNoAdditionalSpace(s));
+        }
+    }
+}
