2dcc8a0f711008edbeaa41d20fb087b60bfb265b
John Krzemien <john.krzemien@gmail.com>
2017-03-14 21:50:55 -0700

    quesiton 1.6 with basic tests

---
+++ Interview/Questions/Cracking the coding interview/Chapter 1/Question_1_6.cs
@@ -0,0 +1,33 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_1
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// String Compression: Implement a method to perform basic string compression
+    /// using the counts of repeated characters. For example, the string
+    /// aabcccccaaa would become a2b1c5a3. If the "compressed" string would not become
+    /// smaller than the original string, your method should return the original
+    /// string. You can assume the string has only uppercase and lowercase letters (a - z).
+    /// </summary>
+    public class Question_1_6
+    {
+        public static string CompressString(string s)
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < s.Length;)
+            {
+                var count = 1;
+                for (var j = i + 1; j < s.Length && s[i] == s[j]; j++, count++)
+                {
+                    // intetionally empty
+                }
+
+                sb.Append("" + s[i] + count);
+                i += count;
+            }
+
+            return sb.ToString().Length < s.Length ? sb.ToString() : s;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 1/Test_1_6.cs
@@ -0,0 +1,18 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_1
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_1;
+
+    [TestClass]
+    public class Test_1_6
+    {
+        [TestMethod]
+        public void Test_1_6_Basic()
+        {
+            Assert.AreEqual("abbb", Question_1_6.CompressString("abbb"));
+            Assert.AreEqual("a1b4", Question_1_6.CompressString("abbbb"));
+            Assert.AreEqual("a1b4c1d3", Question_1_6.CompressString("abbbbcddd"));
+            Assert.AreEqual("abbbbcdd", Question_1_6.CompressString("abbbbcdd"));
+        }
+    }
+}
