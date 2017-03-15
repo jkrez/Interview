e35f338f0cc4280cafe78d421c39b4a2696fdfa0
John Krzemien <john.krzemien@gmail.com>
2017-03-14 20:54:48 -0700

    Adding quesiton 1.5

---
+++ Interview/Questions/Cracking the coding interview/Chapter 1/Question_1_5.cs
@@ -0,0 +1,70 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_1
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+    /// <summary>
+    /// One Away: There are three types of edits that can be performed on strings:
+    /// insert a character, remove a character, or replace a character. Given two
+    /// strings, write a function to check if they are one edit (or zero edits) away.
+    /// 
+    /// Example:
+    /// pale, ple -> true
+    /// pales, pale -> true
+    /// pale, bale -> true
+    /// pale, bake -> false
+    /// </summary>
+    public class Question_1_5
+    {
+        // Time: O(n)
+        // Space: O(1)
+        public static bool IsOneAway(string s1, string s2)
+        {
+            if (string.IsNullOrWhiteSpace(s1) || string.IsNullOrWhiteSpace(s2))
+            {
+                throw new ArgumentException("Input can't be null or empty");
+            }
+
+            if (Math.Max(s1.Length, s2.Length) - Math.Min(s1.Length, s2.Length) > 1)
+            {
+                return false;
+            }
+
+            var equalLength = s1.Length == s2.Length;
+            var operationDone = false;
+            for (int i = 0, j = 0; i < s1.Length && j < s2.Length; i++, j++)
+            {
+                if (i > s2.Length || j > s1.Length || s1[i] != s2[j])
+                {
+                    if (operationDone)
+                    {
+                        return false;
+                    }
+
+                    // "Replace character"
+                    if (equalLength)
+                    {
+                        operationDone = true;
+                    }
+                    else
+                    {
+                        // Remove char from longer string or add character to shorter string
+                        if (s1.Length > s2.Length)
+                        {
+                            i++;
+                        }
+                        else
+                        {
+                            j++;
+                        }
+
+                        operationDone = true;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 1/Test_1_5.cs
@@ -0,0 +1,41 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_1
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_1;
+
+    [TestClass]
+    public class Test_1_5
+    {
+        [TestMethod]
+        public void Question_1_5_Basic()
+        {
+            Assert.AreEqual(true, Question_1_5.IsOneAway("pale", "ple"));
+            Assert.AreEqual(true, Question_1_5.IsOneAway("pales", "pales"));
+            Assert.AreEqual(true, Question_1_5.IsOneAway("pale", "bale"));
+            Assert.AreEqual(false, Question_1_5.IsOneAway("pale", "bake"));
+            Assert.AreEqual(false, Question_1_5.IsOneAway("pale", "ples"));
+            Assert.AreEqual(true, Question_1_5.IsOneAway("pale", "pal"));
+            Assert.AreEqual(true, Question_1_5.IsOneAway("pale", "ale"));
+        }
+
+        [TestMethod]
+        public void Question_1_5_EdgeCase()
+        {
+            Assert.AreEqual(true, Question_1_5.IsOneAway("p", "pp"));
+            Assert.AreEqual(true, Question_1_5.IsOneAway("pp", "p"));
+            Assert.AreEqual(true, Question_1_5.IsOneAway("p", "p"));
+            Assert.AreEqual(true, Question_1_5.IsOneAway("p", "a"));
+            Assert.AreEqual(true, Question_1_5.IsOneAway("ab", "c"));
+        }
+
+        [TestMethod]
+        public void Question_1_5_InvalidCases()
+        {
+            TestHelpers.AssertExceptionThrown(() => Question_1_5.IsOneAway("", "s"), typeof(ArgumentException));
+            TestHelpers.AssertExceptionThrown(() => Question_1_5.IsOneAway(null, "s"), typeof(ArgumentException));
+            TestHelpers.AssertExceptionThrown(() => Question_1_5.IsOneAway(null, null), typeof(ArgumentException));
+            TestHelpers.AssertExceptionThrown(() => Question_1_5.IsOneAway("\t", "s"), typeof(ArgumentException));
+        }
+    }
+}
