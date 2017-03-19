3a074e5baa953288bb8050f2627b58671f803cbe
John Krzemien <john.krzemien@gmail.com>
2017-03-19 11:43:52 -0700

    quesiton 1.9

---
+++ Interview/Questions/Cracking the coding interview/Chapter 1/Question_1_9.cs
@@ -0,0 +1,73 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_1
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    /// String Rotation: Assume you have a method isSubstring which checks
+    /// if one word is a substring of another. Given two strings, 51 and 52,
+    /// write code to check if 52 is a rotation of 51 using only one call to
+    /// isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat").
+    ///
+    /// Assume string.Contains is the call to isSubstring()
+    /// </summary>
+    public class Question_1_9
+    {
+        // Time: max of O(n) and O(isSubstring) - in this case O(n^2)
+        // Spcae: O(n)
+        public static bool IsRotation(string baseString, string rotation)
+        {
+            if (baseString == null || rotation == null)
+            {
+                throw new ArgumentNullException(@"Input string cannot be null.");
+            }
+
+            if (rotation.Length != baseString.Length)
+            {
+                return false;
+            }
+
+            var doubleBase = baseString + baseString;
+            return Question_1_9.IsSubstring(doubleBase, rotation);
+        }
+
+        /// <summary>
+        /// Potential IsSubstring implementation.
+        /// Time: O(m * n)
+        /// Space: O(1)
+        /// </summary>
+        internal static bool IsSubstring(string baseString, string searchString)
+        {
+            if (baseString == null || searchString == null)
+            {
+                throw new ArgumentNullException("Arguments cannot be null");
+            }
+
+            // The empty string should always be found
+            if (searchString.Length == 0)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < baseString.Length; i++)
+            {
+                int j;
+                for (j = 0; j < searchString.Length && i + j < baseString.Length; j++)
+                {
+                    if (baseString[i + j] != searchString[j])
+                    {
+                        break;
+                    }
+                }
+
+                if (j == searchString.Length)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 1/Test_1_9.cs
@@ -0,0 +1,69 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_1
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_1;
+
+    [TestClass]
+    public class Test_1_9
+    {
+        [TestMethod]
+        public void Question_1_9_BasicCases()
+        {
+            var baseString = "abc";
+            var rotatedString = "bca";
+            Assert.AreEqual(true, Question_1_9.IsRotation(baseString, rotatedString));
+
+            baseString = "aaabbb";
+            rotatedString = "bbbaa";
+            Assert.AreEqual(false, Question_1_9.IsRotation(baseString, rotatedString));
+
+            baseString = "aaabbb";
+            rotatedString = "bbbaaa";
+            Assert.AreEqual(true, Question_1_9.IsRotation(baseString, rotatedString));
+
+            baseString = "aaabbb";
+            rotatedString = "bbbaaab";
+            Assert.AreEqual(false, Question_1_9.IsRotation(baseString, rotatedString));
+
+            baseString = "ab";
+            rotatedString = "ba";
+            Assert.AreEqual(true, Question_1_9.IsRotation(baseString, rotatedString));
+
+            baseString = "ab";
+            rotatedString = "bb";
+            Assert.AreEqual(false, Question_1_9.IsRotation(baseString, rotatedString));
+
+            baseString = "aaaaaa";
+            rotatedString = "aaaaa";
+            Assert.AreEqual(false, Question_1_9.IsRotation(baseString, rotatedString));
+
+            baseString = "aaaaa";
+            rotatedString = "aaaaa";
+            Assert.AreEqual(true, Question_1_9.IsRotation(baseString, rotatedString));
+
+            baseString = "aaaaa";
+            rotatedString = "aaaa";
+            Assert.AreEqual(false, Question_1_9.IsRotation(baseString, rotatedString));
+        }
+
+        [TestMethod]
+        public void Question_1_9_EdgeCases()
+        {
+            var baseString = "b";
+            var rotatedString = "b";
+            Assert.AreEqual(true, Question_1_9.IsRotation(baseString, rotatedString));
+
+            baseString = string.Empty;
+            rotatedString = string.Empty;
+            Assert.AreEqual(true, Question_1_9.IsRotation(baseString, rotatedString));
+        }
+
+        [TestMethod]
+        public void Question_1_9_InvaidCases()
+        {
+            TestHelpers.AssertExceptionThrown(() => Question_1_9.IsRotation(null, string.Empty), typeof(ArgumentNullException));
+            TestHelpers.AssertExceptionThrown(() => Question_1_9.IsRotation(string.Empty, null), typeof(ArgumentNullException));
+        }
+    }
+}
