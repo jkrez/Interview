84c5d5fd70f19dc39dd4bf3cc052261edff709db
John Krzemien <john.krzemien@gmail.com>
2017-03-21 14:30:55 -0700

    question 2.1 start

--- Interview/Questions/Cracking the coding interview/Chapter 2/Question_2_1.cs
+++ Interview/Questions/Cracking the coding interview/Chapter 2/Question_2_1.cs
@@ -1,5 +1,9 @@
 ﻿namespace Questions.Cracking_the_coding_interview.Chapter_2
 {
+    using System;
+    using System.Collections.Generic;
+    using Questions.Data_structures;
+
     /// <summary>
     /// Remove Dups: Write code to remove duplicates from an unsorted linked list.
     /// FOLLOW UP
@@ -7,9 +11,35 @@
     /// </summary>
     public class Question_2_1
     {
-        public static void RemoveDuplicates()
+        // Time: O(n)
+        // Space: O(n)
+        public static Node<T> RemoveDuplicates<T>(Node<T> head)
+            where T : IEquatable<T>
         {
-            
+            HashSet<T> nodes = new HashSet<T>();
+            Node<T> noDuplicates = null;
+
+            while (head != null)
+            {
+                if (!nodes.Contains(head.Data))
+                {
+                    nodes.Add(head.Data);
+                    var toInsert = new Node<T>(head.Data);
+                    // Case for first entry
+                    if (noDuplicates == null)
+                    {
+                        noDuplicates = toInsert;
+                    }
+                    else
+                    {
+                        noDuplicates.Next = toInsert;
+                        noDuplicates = toInsert;
+                    }
+                    head = head.Next;
+                }
+            }
+
+            return noDuplicates;
         }
     }
 }
---
+++ Interview/Tests/ListHelpers.cs
@@ -0,0 +1,13 @@
+﻿namespace Tests
+{
+    using System.Runtime.Remoting.Messaging;
+    using Questions.Data_structures;
+
+    public class ListHelpers
+    {
+        public static Node<int>CreateLinkedList(params int[] list)
+        {
+            
+        }
+    }
+}
