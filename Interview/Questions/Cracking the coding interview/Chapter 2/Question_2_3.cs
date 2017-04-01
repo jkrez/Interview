513649b24dd75ac0be5a0c79f0db3cd78004e248
John Krzemien <john.krzemien@gmail.com>
2017-04-01 15:22:46 -0700

    Adding question 2.3

---
+++ Interview/Questions/Cracking the coding interview/Chapter 2/Question_2_3.cs
@@ -0,0 +1,34 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_2
+{
+    using System;
+    using Data_structures;
+
+    /// <summary>
+    /// Delete Middle Node: Implement an algorithm to delete a node in the middle
+    /// (i.e., any node but the first and last node, not necessarily the exact middle)
+    /// of a singly linked list, given only access to that node.
+    /// 
+    /// EXAMPLE
+    /// Input: the node c from the linked list a -> b -> c -> d -> e -> f
+    /// Result: nothing is returned, but the new linked list looks like a -> b -> d -> e -> f
+    /// </summary>
+    public class Question_2_3
+    {
+        public static void RemoveMiddleNode<T>(Node<T> node)
+            where T : IEquatable<T>
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
+
+            if (node.Next == null)
+            {
+                throw new ArgumentException("node should be inner node in linked list");
+            }
+
+            node.Data = node.Next.Data;
+            node.Next = node.Next.Next;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 2/Test_2_3.cs
@@ -0,0 +1,47 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_2
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_2;
+    using Questions.Data_structures;
+
+    [TestClass]
+    public class Test_2_3
+    {
+        [TestMethod]
+        public void Question_2_3_BasicCases()
+        {
+            // Remove 1 middle node: 1, 2, 3 => 1, 3
+            var l1 = ListHelpers.CreateLinkedList(1, 2, 3);
+            var expected = ListHelpers.CreateLinkedList(1, 3);
+            Question_2_3.RemoveMiddleNode(l1.Next);
+            ListHelpers.ValidateLinkedListContent(expected, l1);
+
+            // Remove 2: 1, 2, 3, 4 => 1, 4
+            var l2 = ListHelpers.CreateLinkedList(1, 2, 3, 4);
+            expected = ListHelpers.CreateLinkedList(1, 4);
+            Question_2_3.RemoveMiddleNode(l2.Next);
+            Question_2_3.RemoveMiddleNode(l2.Next);
+            ListHelpers.ValidateLinkedListContent(expected, l2);
+
+            // Remove 1 node from big list: 1, 2, 3, 4, 5, 6, 7 => 1, 2, 3, 5, 6, 7
+            var l3 = ListHelpers.CreateLinkedList(1, 2, 3, 4, 5, 6, 7);
+            expected = ListHelpers.CreateLinkedList(1, 2, 3, 5, 6, 7);
+            Question_2_3.RemoveMiddleNode(l3.Next.Next.Next);
+            ListHelpers.ValidateLinkedListContent(expected, l3);
+        }
+
+        [TestMethod]
+        public void Question_2_3_InvalidCases()
+        {
+            // Remove end of list
+            var l3 = ListHelpers.CreateLinkedList(1, 2);
+            var expected = ListHelpers.CreateLinkedList(1);
+            TestHelpers.AssertExceptionThrown(() => Question_2_3.RemoveMiddleNode(l3.Next), typeof(ArgumentException));
+
+            // Pass null node.
+            Node<int> head = null;
+            TestHelpers.AssertExceptionThrown(() => Question_2_3.RemoveMiddleNode(head), typeof(ArgumentNullException));
+        }
+    }
+}
