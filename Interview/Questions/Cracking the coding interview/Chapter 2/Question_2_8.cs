20e8a9493eea6cf87195b89c63657096cbb069ca
John Krzemien <john.krzemien@gmail.com>
2017-04-08 19:11:22 -0700

    Question 2.8

---
+++ Interview/Questions/Cracking the coding interview/Chapter 2/Question_2_8.cs
@@ -0,0 +1,56 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_2
+{
+    using System;
+    using Data_structures;
+
+    /// <summary>
+    /// Loop Detection: Given a circular linked list, implement an algorithm
+    /// that returns the node at the beginning of the loop.
+    ///
+    /// DEFINITION
+    /// Circular linked list: A (corrupt) linked list in which a node's next
+    /// pointer points to an earlier node, so as to make a loop in the linked
+    /// list.
+    ///
+    /// EXAMPLE
+    /// Input: A -> B -> C -> D -> E -> C (the same C as earlier)
+    ///
+    /// Output: C
+    /// </summary>
+    public class Question_2_8
+    {
+        // Floyd's algorithm
+        // Space: O(1)
+        // Time: O(n + m)
+        public static Node<T> BeginningOfCycle<T>(Node<T> head)
+             where T : IEquatable<T>
+        {
+            if (head == null)
+            {
+                throw new ArgumentNullException(nameof(head));
+            }
+
+            var runner = head;
+            var runnerFast = head;
+            do
+            {
+                if (runnerFast.Next == null || runnerFast.Next.Next == null)
+                {
+                    throw new ArgumentException(@"No cycle in list.");
+                }
+
+                runner = runner.Next;
+                runnerFast = runnerFast.Next.Next;
+            } while (runner != runnerFast);
+
+            runner = head;
+            while (runner != runnerFast)
+            {
+                runner = runner.Next;
+                runnerFast = runnerFast.Next;
+            }
+
+            return runner;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 2/Test_2_8.cs
@@ -0,0 +1,69 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_2
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_2;
+    using Questions.Data_structures;
+
+    [TestClass]
+    public class Test_2_8
+    {
+        [TestMethod]
+        public void Question_2_8_BasicCases()
+        {
+            var list = ListHelpers.CreateLinkedList(1, 2, 3);
+            var loopStart = AddLoop(list, 4, 5, 6);
+            Validate(loopStart, list);
+
+            list = ListHelpers.CreateLinkedList(1);
+            loopStart = AddLoop(list, 4, 5, 6);
+            Validate(loopStart, list);
+
+            list = ListHelpers.CreateLinkedList(1);
+            loopStart = AddLoop(list, 4);
+            Validate(loopStart, list);
+
+            loopStart = AddLoop(null, 5);
+            Validate(loopStart, loopStart);
+
+        }
+
+        [TestMethod]
+        public void Question_2_8_InvalidCases()
+        {
+            TestHelpers.AssertExceptionThrown(() => Question_2_8.BeginningOfCycle<int>(null), typeof(ArgumentNullException));
+        }
+
+        private static void Validate<T>(Node<T> expected, Node<T> list)
+            where T : IEquatable<T>
+        {
+            Assert.AreEqual(expected, Question_2_8.BeginningOfCycle(list));
+        }
+
+        private static Node<T> AddLoop<T>(Node<T> head, params T[] loop)
+         where T : IEquatable<T>
+        {
+            while (head?.Next != null)
+            {
+                head = head.Next;
+            }
+
+            var loopStart = ListHelpers.CreateLinkedList(loop);
+            var loopEnd = loopStart;
+
+            while (loopEnd.Next != null)
+            {
+                loopEnd = loopEnd.Next;
+            }
+
+            if (head != null)
+            {
+                head.Next = loopStart;
+            }
+
+            loopEnd.Next = loopStart;
+
+            return loopStart;
+        }
+    }
+}
