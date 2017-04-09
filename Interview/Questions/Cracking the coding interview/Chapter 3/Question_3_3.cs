2497557006cfa86b6ce39568250519b79b71d7da
John Krzemien <john.krzemien@gmail.com>
2017-04-09 12:07:32 -0700

    Add question 3.3 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 3/Question_3_3.cs
@@ -0,0 +1,68 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Stack of Plates: Imagine a (literal) stack of plates. If the stack gets
+    /// too high, it might topple. Therefore, in real life, we would likely
+    /// start a new stack when the previous stack exceeds some threshold.
+    /// Implement a data structure SetOfStacks that mimics this. SetOfStacks
+    /// should be composed of several stacks and should create a new stack once
+    /// the previous one exceeds capacity. SetOfStacks. push () and SetOfStacks.
+    /// pop () should behave identically to a single stack (that is, pop ( )
+    /// should return the same values as it would if there were just a single
+    /// stack).
+    ///
+    /// FOLLOW UP
+    /// Implement a function popAt(int index) which performs a pop operation on
+    /// a specific sub-stack.
+    /// </summary>
+    [SuppressMessage("Documentation Rules", "SA1649")]
+    [SuppressMessage("Microsoft.Naming", "CA1711")]
+    public class SetOfStacks<T>
+        where T : IComparable<T>
+    {
+        private const int maxSizePerStack = 2;
+        private readonly List<T[]> stacks;
+        private int numItems;
+
+        public SetOfStacks()
+        {
+            stacks = new List<T[]>();
+        }
+
+        public void Push(T item)
+        {
+            var stackNumber = this.numItems / maxSizePerStack;
+            var stackIndex = this.numItems % maxSizePerStack;
+            if (stackIndex == 0)
+            {
+                this.stacks.Add(new T[maxSizePerStack]);
+            }
+
+            this.stacks[stackNumber][stackIndex] = item;
+            this.numItems++;
+        }
+
+        public T Pop()
+        {
+            this.numItems--;
+            var stackNumber = this.numItems / maxSizePerStack;
+            var stackIndex = this.numItems % maxSizePerStack;
+            var item = this.stacks[stackNumber][stackIndex];
+            if (stackIndex == 0)
+            {
+                this.stacks.Remove(this.stacks[stackNumber]);
+            }
+
+            return item;
+        }
+
+        public T PopAt(int index)
+        {
+            return default(T);
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 3/Test_3_3.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_3
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_3;
+
+    [TestClass]
+    public class Test_3_3
+    {
+        [TestMethod]
+        public void Question_3_3_BasicCases()
+        {
+            var item1 = 1;
+            var item2 = 2;
+            var item3 = 3;
+            var item4 = 0;
+            var item5 = -1;
+
+            var stackSet = new SetOfStacks<int>();
+            stackSet.Push(item1);
+            stackSet.Push(item2);
+            stackSet.Push(item3);
+            stackSet.Push(item4);
+            stackSet.Push(item5);
+            var pop1 = stackSet.Pop();
+            var pop2 = stackSet.Pop();
+            var pop3 = stackSet.Pop();
+            var pop4 = stackSet.Pop();
+            var pop5 = stackSet.Pop();
+
+            Assert.AreEqual(pop1, item5);
+            Assert.AreEqual(pop2, item4);
+            Assert.AreEqual(pop3, item3);
+            Assert.AreEqual(pop4, item2);
+            Assert.AreEqual(pop5, item1);
+        }
+    }
+}
