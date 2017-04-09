6318d549f96e4039d2749cfc10a43b7588831dc6
John Krzemien <john.krzemien@gmail.com>
2017-04-09 10:39:40 -0700

    Question 3.2 sln

---
+++ Interview/Questions/Cracking the coding interview/Chapter 3/Question_3_2.cs
@@ -0,0 +1,73 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_3
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Stack Min: How would you design a stack which, in addition to
+    /// push and pop, has a function min which returns the minimum element?
+    /// Push, pop and min should all operate in 0(1) time.
+    /// </summary>
+    [SuppressMessage("Documentation Rules", "SA1649")]
+    [SuppressMessage("Microsoft.Naming", "CA1711")]
+    public class MinStack<T>
+        where T : IComparable<T>
+    {
+        // A better soloution would be where this grows dynamically.
+        private const int maxItems = 100;
+
+        private readonly T[] stackItems;
+        private readonly T[] minItems;
+
+        private int numItems;
+        private int numMinItems;
+
+        public MinStack()
+        {
+            this.stackItems = new T[maxItems];
+            this.minItems = new T[maxItems];
+        }
+
+        public void Push(T item)
+        {
+            if (this.numItems == maxItems)
+            {
+                throw new InvalidOperationException(@"Stack is full.");
+            }
+
+            this.stackItems[this.numItems++] = item;
+            if (this.numMinItems == 0 ||
+                this.minItems[this.numMinItems].CompareTo(item) >= 0)
+            {
+                this.minItems[this.numMinItems++] = item;
+            }
+        }
+
+        public T Pop()
+        {
+            if (numItems == 0)
+            {
+                throw new InvalidOperationException(@"Stack does not contain any items.");
+            }
+
+            var itemRemoved = this.stackItems[this.numItems - 1];
+            if (itemRemoved.CompareTo(this.GetMinimum()) == 0)
+            {
+                this.numMinItems--;
+            }
+
+            this.numItems--;
+            return itemRemoved;
+        }
+
+        public T GetMinimum()
+        {
+            if (numItems == 0)
+            {
+                throw new InvalidOperationException(@"Stack does not contain any items.");
+            }
+
+            return this.minItems[this.numMinItems - 1];
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 3/Test_3_2.cs
@@ -0,0 +1,73 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_3
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_3;
+
+    [TestClass]
+    public class Test_3_2
+    {
+        [TestMethod]
+        public void Question_3_2_BasicCases()
+        {
+            var item1 = 1;
+            var item2 = 2;
+            var item3 = 3;
+            var item4 = 0;
+            var item5 = -1;
+
+            var ms1 = new MinStack<int>();
+            ms1.Push(item1);
+            var min1 = ms1.GetMinimum();
+            ms1.Push(item2);
+            var min2 = ms1.GetMinimum();
+            ms1.Push(item3);
+            var min3 = ms1.GetMinimum();
+            ms1.Push(item4);
+            var min4 = ms1.GetMinimum();
+            ms1.Push(item5);
+            var min5 = ms1.GetMinimum();
+            var pop1 = ms1.Pop();
+            var min6 = ms1.GetMinimum();
+            var pop2 = ms1.Pop();
+            var min7 = ms1.GetMinimum();
+            var pop3 = ms1.Pop();
+            var min8 = ms1.GetMinimum();
+            var pop4 = ms1.Pop();
+            var min9 = ms1.GetMinimum();
+            var pop5 = ms1.Pop();
+
+            Assert.AreEqual(pop1, item5);
+            Assert.AreEqual(pop2, item4);
+            Assert.AreEqual(pop3, item3);
+            Assert.AreEqual(pop4, item2);
+            Assert.AreEqual(pop5, item1);
+
+            Assert.AreEqual(1, min1);
+            Assert.AreEqual(1, min2);
+            Assert.AreEqual(1, min3);
+            Assert.AreEqual(0, min4);
+            Assert.AreEqual(-1, min5);
+            Assert.AreEqual(0, min6);
+            Assert.AreEqual(1, min7);
+            Assert.AreEqual(1, min8);
+            Assert.AreEqual(1, min9);
+        }
+
+        [TestMethod]
+        public void Question_3_2_InvalidCases()
+        {
+            var ms1 = new MinStack<int>();
+
+            TestHelpers.AssertExceptionThrown(() => { ms1.Pop(); }, typeof(InvalidOperationException));
+            TestHelpers.AssertExceptionThrown(() => { ms1.GetMinimum(); }, typeof(InvalidOperationException));
+
+            for (int i = 100; i > 0; i--)
+            {
+                ms1.Push(i);
+            }
+
+            TestHelpers.AssertExceptionThrown(() => { ms1.Push(1); }, typeof(InvalidOperationException));
+        }
+    }
+}
