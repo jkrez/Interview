df93ef5116101c3d95fa820c6abf50e23083aed8
John Krzemien <john.krzemien@gmail.com>
2017-04-08 17:11:14 -0700

    question 2.7

---
+++ Interview/Questions/Cracking the coding interview/Chapter 2/Question_2_7.cs
@@ -0,0 +1,55 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_2
+{
+    using System;
+    using Data_structures;
+
+    /// <summary>
+    /// Intersection: Given two (singly) linked lists, determine if the two
+    /// lists intersect. Return the intersecting node. Note that the intersection
+    /// is defined based on reference, not value. That is, if the kth node of
+    /// the first linked list is the exact same node (by reference) as the jth
+    /// node of the second linked list, then they are intersecting. 
+    /// </summary>
+    public class Question_2_7
+    {
+        public static Node<T> FindIntersection<T>(Node<T> head1, Node<T> head2)
+            where T : IEquatable<T>
+        {
+            if (head1 == null)
+            {
+                throw new ArgumentNullException(nameof(head1));
+            }
+
+            if (head2 == null)
+            {
+                throw new ArgumentNullException(nameof(head2));
+            }
+
+            var length1 = Question_2_2.GetListLength(head1);
+            var length2 = Question_2_2.GetListLength(head2);
+
+            var diff = Math.Abs(length1 - length2);
+            var longerHead = length1 > length2 ? head1 : head2;
+            var shorterHead = length1 <= length2 ? head1 : head2;
+
+            for (int i = 0; i < diff; i++)
+            {
+                longerHead = longerHead.Next;
+            }
+
+            while (longerHead != null && shorterHead != null)
+            {
+                if (ReferenceEquals(longerHead, shorterHead))
+                {
+                    return longerHead;
+                }
+
+                longerHead = longerHead.Next;
+                shorterHead = shorterHead.Next;
+            }
+
+            // Depending on the use case, should consider throwing and exception here.
+            return null;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 2/Test_2_7.cs
@@ -0,0 +1,75 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_2
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_2;
+    using Questions.Data_structures;
+
+    [TestClass]
+    public class Test_2_7
+    {
+        [TestMethod]
+        public void Question_2_7_BasicCases()
+        {
+            var l1 = ListHelpers.CreateLinkedList(1, 2);
+            var l2 = ListHelpers.CreateLinkedList(3, 4);
+            var intersection = ListHelpers.CreateLinkedList(5, 6);
+            AddIntersection(l1, l2, intersection);
+            Validate(intersection, l1, l2);
+
+            l1 = ListHelpers.CreateLinkedList(1);
+            l2 = ListHelpers.CreateLinkedList(2);
+            intersection = ListHelpers.CreateLinkedList(3);
+            AddIntersection(l1, l2, intersection);
+            Validate(intersection, l1, l2);
+
+            l1 = ListHelpers.CreateLinkedList(1, 2, 3, 4);
+            l2 = ListHelpers.CreateLinkedList(2);
+            intersection = ListHelpers.CreateLinkedList(5);
+            AddIntersection(l1, l2, intersection);
+            Validate(intersection, l1, l2);
+
+            l1 = ListHelpers.CreateLinkedList(1, 2, 3, 4);
+            l2 = ListHelpers.CreateLinkedList(2, 2, 2, 2);
+            intersection = ListHelpers.CreateLinkedList(5, 4, 3, 2, 3);
+            AddIntersection(l1, l2, intersection);
+            Validate(intersection, l1, l2);
+
+            intersection = ListHelpers.CreateLinkedList(4);
+            Validate(intersection, intersection, intersection);
+
+        }
+
+        [TestMethod]
+        public void Question_2_7_InvalidCases()
+        {
+            TestHelpers.AssertExceptionThrown(() => Question_2_7.FindIntersection(null, new Node<int>(1)),
+                typeof(ArgumentNullException));
+            TestHelpers.AssertExceptionThrown(() => Question_2_7.FindIntersection(new Node<int>(1), null),
+                typeof(ArgumentNullException));
+        }
+
+        private static void Validate<T>(Node<T> expected, Node<T> l1, Node<T> l2)
+            where T : IEquatable<T>
+        {
+            Assert.IsTrue(ReferenceEquals(Question_2_7.FindIntersection(l1, l2), expected));
+        }
+
+        private static void AddIntersection<T>(Node<T> l1, Node<T> l2, Node<T> intersection)
+            where T : IEquatable<T>
+        {
+            while (l1.Next != null)
+            {
+                l1 = l1.Next;
+            }
+
+            while (l2.Next != null)
+            {
+                l2 = l2.Next;
+            }
+
+            l1.Next = intersection;
+            l2.Next = intersection;
+        }
+    }
+}
