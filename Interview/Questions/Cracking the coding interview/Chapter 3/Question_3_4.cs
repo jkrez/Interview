dbb1e024f9388557cedcf8418eff2f71d10ce142
John Krzemien <john.krzemien@gmail.com>
2017-04-09 12:31:46 -0700

    Add question 3.4 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 3/Question_3_4.cs
@@ -0,0 +1,63 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Queue via Stacks: Implement a MyQueue class which implements a
+    /// queue using two stacks.
+    /// </summary>
+    [SuppressMessage("Documentation Rules", "SA1649")]
+    [SuppressMessage("Microsoft.Naming", "CA1711")]
+    public class QueueFromStacks<T>
+        where T : IComparable<T>
+    {
+        private readonly Stack<T> stackPushTo;
+        private readonly Stack<T> stackPopFrom;
+        private int numItems;
+
+        private bool itemsInPushStack => this.stackPopFrom.Count == 0;
+
+        public QueueFromStacks()
+        {
+            this.stackPushTo = new Stack<T>();
+            this.stackPopFrom = new Stack<T>();
+        }
+
+        public void Push(T item)
+        {
+            if (!this.itemsInPushStack)
+            {
+                this.MoveItemsToStack(this.stackPushTo, this.stackPopFrom);
+            }
+
+            this.stackPushTo.Push(item);
+            this.numItems++;
+        }
+
+        public T Pop()
+        {
+            if (this.numItems == 0)
+            {
+                throw new InvalidOperationException(@"There are no items in the stack.");
+            }
+
+            if (this.itemsInPushStack)
+            {
+                this.MoveItemsToStack(this.stackPopFrom, this.stackPushTo);
+            }
+
+            this.numItems--;
+            return this.stackPopFrom.Pop();
+        }
+
+        private void MoveItemsToStack(Stack<T> dest, Stack<T> src)
+        {
+            while (src.Count != 0)
+            {
+                dest.Push(src.Pop());
+            }
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 3/Test_3_4.cs
@@ -0,0 +1,46 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_3
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_3;
+
+    [TestClass]
+    public class Test_3_4
+    {
+        [TestMethod]
+        public void Question_3_4_BasicCases()
+        {
+            var item1 = 1;
+            var item2 = 2;
+            var item3 = 3;
+            var item4 = 0;
+            var item5 = -1;
+
+            var qs = new QueueFromStacks<int>();
+            qs.Push(item1);
+            qs.Push(item2);
+            qs.Push(item3);
+            qs.Push(item4);
+            qs.Push(item5);
+            var pop1 = qs.Pop();
+            var pop2 = qs.Pop();
+            var pop3 = qs.Pop();
+            var pop4 = qs.Pop();
+            var pop5 = qs.Pop();
+
+            Assert.AreEqual(pop1, item1);
+            Assert.AreEqual(pop2, item2);
+            Assert.AreEqual(pop3, item3);
+            Assert.AreEqual(pop4, item4);
+            Assert.AreEqual(pop5, item5);
+        }
+
+        [TestMethod]
+        public void Question_3_4_InvalidCases()
+        {
+            var stack = new QueueFromStacks<int>();
+
+            TestHelpers.AssertExceptionThrown(() => { stack.Pop(); }, typeof(InvalidOperationException));
+        }
+    }
+}
