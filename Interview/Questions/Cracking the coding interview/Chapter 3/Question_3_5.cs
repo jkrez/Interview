605f3c3a596891c113028f0c9a92b0449e4aa2c3
John Krzemien <john.krzemien@gmail.com>
2017-04-10 20:35:43 -0700

    Add question 3.5 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 3/Question_3_5.cs
@@ -0,0 +1,51 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_3
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Sort Stack: Write a program to sort a stack such that the smallest items
+    /// are on the top. You can use an additional temporary stack, but you may
+    /// not copy the elements into any other data structure (such as an array).
+    /// The stack supports the following operations: push, pop, peek, and isEmpty.
+    /// </summary>
+    public class Question_3_5
+    {
+        // Space: O(n)
+        // Time: O(n^2)
+        public static void SortStack<T>(Stack<T> stack)
+            where T : IComparable<T>
+        {
+            if (stack == null)
+            {
+                throw new ArgumentNullException(nameof(stack));
+            }
+
+            if (stack.Count == 0)
+            {
+                return;
+            }
+
+            var helperStack = new Stack<T>();
+
+            // Move all of stack to helperStack
+            while (stack.Count != 0)
+            {
+                helperStack.Push(stack.Pop());
+            }
+
+            stack.Push(helperStack.Pop());
+            while (helperStack.Count != 0)
+            {
+                var next = helperStack.Pop();
+                while (stack.Count != 0 &&
+                       stack.Peek().CompareTo(next) < 0)
+                {
+                    helperStack.Push(stack.Pop());
+                }
+
+                stack.Push(next);
+            }
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 3/Test_3_5.cs
@@ -0,0 +1,74 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_3
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_3;
+
+    [TestClass]
+    public class Test_3_5
+    {
+        [TestMethod]
+        public void Question_3_5_BasicCases()
+        {
+            var s1 = new Stack<int>();
+            s1.Push(1);
+            s1.Push(2);
+            s1.Push(3);
+            s1.Push(4);
+            s1.Push(5);
+            Validate(s1);
+
+            var s2 = new Stack<int>();
+            s2.Push(4);
+            s2.Push(3);
+            s2.Push(5);
+            s2.Push(4);
+            s2.Push(3);
+            s2.Push(5);
+            s2.Push(4);
+            s2.Push(4);
+            Validate(s2);
+
+            var s3 = new Stack<int>();
+            s3.Push(1);
+            Validate(s3);
+
+            var s4 = new Stack<int>();
+            s4.Push(2);
+            s4.Push(1);
+            s4.Push(3);
+            Validate(s4);
+
+            Validate(new Stack<int>());
+        }
+
+        [TestMethod]
+        public void Question_3_5_InvalidCases()
+        {
+            Stack<int> s = null;
+            TestHelpers.AssertExceptionThrown(() => Question_3_5.SortStack(s), typeof(ArgumentNullException));
+        }
+
+        private static void Validate<T>(Stack<T> stack)
+            where T : IComparable<T>
+        {
+            var cloneStack = new Stack<T>(stack);
+            Question_3_5.SortStack(cloneStack);
+            ValidateOrder(cloneStack);
+        }
+
+        private static void ValidateOrder<T>(Stack<T> s)
+            where T : IComparable<T>
+        {
+            while (s.Count != 0)
+            {
+                var next = s.Pop();
+                if (s.Count != 0)
+                {
+                    Assert.IsTrue(s.Peek().CompareTo(next) >= 0);
+                }
+            }
+        }
+    }
+}
