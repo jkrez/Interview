23319c765002446be653ad79ba9db906ea351d1c
John Krzemien <john.krzemien@gmail.com>
2017-04-29 09:07:48 -0700

    Adding question 4.5 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 4/Question_4_5.cs
@@ -0,0 +1,45 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_4
+{
+    using System;
+    using Data_structures;
+
+    public class Question_4_5
+    {
+        public static bool IsBST(BinaryTreeNode<int> root)
+        {
+            if (root == null)
+            {
+                throw new ArgumentNullException(nameof(root));
+            }
+
+            // Note that if this was to be generic IsBST<T> would require a
+            // class type that implemented IComparable and IEquatable and
+            // the min and max values could be null until set.
+            return IsBSTHelper(root, int.MinValue, int.MaxValue);
+        }
+
+        private static bool IsBSTHelper(BinaryTreeNode<int> root, int min, int max)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            // This allows duplicates to be on either side of the tree.
+            if (root.Data > max ||
+                root.Data < min)
+            {
+                return false;
+            }
+
+            var leftTreeValid = IsBSTHelper(root.Left, min, root.Data);
+            if (!leftTreeValid)
+            {
+                return false;
+            }
+
+            var rightValid = IsBSTHelper(root.Right, root.Data, max);
+            return rightValid;
+        }
+    }
+}
