2cfe63ed9d3df6f89be638f2506472e036280869
John Krzemien <john.krzemien@gmail.com>
2017-04-29 08:21:10 -0700

    Adding question 4.4 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 4/Question_4_4.cs
@@ -0,0 +1,47 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_4
+{
+    using System;
+    using Questions.Data_structures;
+
+    public class Question_4_4
+    {
+        public static bool IsBalanced<T>(BinaryTreeNode<T> root)
+            where T : IEquatable<T>
+        {
+            if (root == null)
+            {
+                throw new ArgumentNullException(nameof(root));
+            }
+
+            int maxDepth;
+            return IsBalancedHelper(root, /* height */ 0, out maxDepth);
+        }
+
+        private static bool IsBalancedHelper<T>(BinaryTreeNode<T> root, int height, out int maxDepth)
+            where T : IEquatable<T>
+        {
+            maxDepth = height;
+            if (root == null)
+            {
+                return true;
+            }
+
+            int leftDepth;
+            var leftBalanced = IsBalancedHelper(root.Left, height + 1, out leftDepth);
+            if (!leftBalanced)
+            {
+                return false;
+            }
+
+            int rightDepth;
+            var rightBalanced = IsBalancedHelper(root.Right, height + 1, out rightDepth);
+            if (!rightBalanced)
+            {
+                return false;
+            }
+
+            maxDepth = Math.Max(leftDepth, rightDepth);
+            return Math.Abs(leftDepth - rightDepth) <= 1;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 4/Test_4_4.cs
@@ -0,0 +1,52 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_4
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_4;
+    using Questions.Data_structures;
+
+    [TestClass]
+    public class Test_4_4
+    {
+
+        [TestMethod]
+        public void Question_4_4_BasicCases()
+        {
+            // One node tree
+            var tree = TreeHelpers.CreateBinaryTree(1, null, null);
+            Validate(true, tree);
+
+            // Two level tree
+            tree = TreeHelpers.CreateBinaryTree(1, 2, 0);
+            Validate(true, tree);
+
+            // Two node uneven tree.
+            tree = TreeHelpers.CreateBinaryTree(
+                1,
+                TreeHelpers.CreateBinaryTree(2, new BinaryTreeNode<int>(3), null),
+                null);
+
+            Validate(false, tree);
+
+            // Non BST very uneven.
+            var temp1 = TreeHelpers.CreateBinaryTree(0, 1, 2);
+            var temp2 = TreeHelpers.CreateBinaryTree(4, 5, 6);
+            var temp3 = TreeHelpers.CreateBinaryTree(11, temp1, temp2);
+            tree = TreeHelpers.CreateBinaryTree(8, temp3, null);
+            Validate(false, tree);
+        }
+
+        [TestMethod]
+        public void Question_4_4_InvalidCases()
+        {
+            TestHelpers.AssertExceptionThrown(() => Question_4_4.IsBalanced((BinaryTreeNode<int>)null), typeof(ArgumentNullException));
+        }
+
+        private static void Validate<T>(bool expected, BinaryTreeNode<T> root)
+            where T : IEquatable<T>
+        {
+            var result = Question_4_4.IsBalanced(root);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
