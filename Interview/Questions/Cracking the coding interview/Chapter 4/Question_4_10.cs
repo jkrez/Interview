2a574776f59ce6f77be2f02a12e045c56e684658
John Krzemien <john.krzemien@gmail.com>
2017-05-07 20:35:17 -0700

    Adding question 4.10 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 4/Question_4_10.cs
@@ -0,0 +1,60 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_4
+{
+    using System;
+    using Data_structures;
+
+    public class Question_4_10
+    {
+        public static bool ContainsSubtree<T>(BinaryTreeNode<T> t1, BinaryTreeNode<T> t2)
+            where T : IEquatable<T>
+        {
+            if (t1 == null && t2 == null)
+            {
+                return true;
+            }
+
+            if (t1 == null)
+            {
+                return false;
+            }
+
+            if (IsSubtree(t1, t2))
+            {
+                return true;
+            }
+
+            if (ContainsSubtree<T>(t1.Left, t2))
+            {
+                return true;
+            }
+
+            return ContainsSubtree<T>(t1.Right, t2);
+        }
+
+        private static bool IsSubtree<T>(BinaryTreeNode<T> node, BinaryTreeNode<T> t2)
+            where T : IEquatable<T>
+        {
+            if (node == null)
+            {
+                return t2 == null; // Nothing left to match
+            }
+
+            if (t2 == null)
+            {
+                return false;
+            }
+
+            if (!t2.Data.Equals(node.Data))
+            {
+                return false;
+            }
+
+            if (!IsSubtree<T>(node.Left, t2.Left))
+            {
+                return false;
+            }
+
+            return IsSubtree<T>(node.Right, t2.Right);
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 4/Test_4_10.cs
@@ -0,0 +1,65 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_4
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_4;
+    using Questions.Data_structures;
+    using Questions.LeetCode;
+
+    [TestClass]
+    public class Test_4_10
+    {
+        [TestMethod]
+        public void Question_4_10_BasicCases()
+        {
+            // Null tree
+            var t1 = (BinaryTreeNode<int>) null;
+            var t2 = t1;
+            Validate(t1, t2, expected: true);
+            t2 = new BinaryTreeNode<int>(1);
+            Validate(t1, t2, expected: false);
+
+            // Single Node tree tests.
+            t1 = TreeHelpers.CreateBinaryTree(1, null, null);
+            t2 = (BinaryTreeNode<int>) null;
+            Validate(t1, t2, expected: true);
+            t2 = new BinaryTreeNode<int>(1);
+            Validate(t1, t2, expected: true);
+            t2 = new BinaryTreeNode<int>(2);
+            Validate(t1, t2, expected: false);
+
+            // Small tree tests.
+            t1 = TreeHelpers.CreateBinaryTree(1, new BinaryTreeNode<int>(0), null);
+            t2 = t1.Left;
+            Validate(t1, t2, expected: true);
+            Validate(t1, new BinaryTreeNode<int>(4), expected: false);
+            Validate(t1, TreeHelpers.CreateBinaryTree(1, 0, 0), expected: false);
+            t2 = TreeHelpers.CreateBinaryTree(1, new BinaryTreeNode<int>(0), null);
+            Validate(t1, t2, expected: true);
+
+            // Larger tree tests
+            t1 = TreeHelpers.CreateBinaryTree(9, new BinaryTreeNode<int>(8), t1);
+            t2 = TreeHelpers.CreateBinaryTree(9, new BinaryTreeNode<int>(8), t2);
+            Validate(t1, t2, expected: true);
+            Validate(t1, t2.Left, expected: true);
+            Validate(t1, t2.Right, expected: true);
+            Validate(t1, t2.Right.Right, expected: true);
+            Validate(t1, TreeHelpers.CreateBinaryTree(8, new BinaryTreeNode<int>(9), null), expected: false);
+
+            // Medium tree tests
+            t1 = TreeHelpers.CreateBinaryTree(
+                data: 4,
+                left: TreeHelpers.CreateBinaryTree(2, 1, 10),
+                right: TreeHelpers.CreateBinaryTree(6, 5, 7));
+            t2 = TreeHelpers.CreateBinaryTree(2, 1, 10);
+            Validate(t1, t2, expected: true);
+            Validate(t1, TreeHelpers.CreateBinaryTree(5, 6, 7), expected: false);
+        }
+
+        private static void Validate(BinaryTreeNode<int> t1, BinaryTreeNode<int> t2, bool expected)
+        {
+            var result = Question_4_10.ContainsSubtree(t1, t2);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
