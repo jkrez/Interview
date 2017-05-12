198591d69346ee26f6d9baedcb402a02a7bbd96a
John Krzemien <john.krzemien@gmail.com>
2017-05-11 18:46:26 -0700

    Adding question 4.12 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 4/Question_4_12.cs
@@ -0,0 +1,47 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_4
+{
+    using System;
+    using System.Collections.Generic;
+    using Data_structures;
+
+    public class Question_4_12
+    {
+        public static int SumPaths(BinaryTreeNode<int> node, int target)
+        {
+            var sums = new Dictionary<Tuple<int, int>, int>();
+            return SumPathsHelper(node, target, 0, sums);
+        }
+
+        private static int SumPathsHelper(
+            BinaryTreeNode<int> node,
+            int target,
+            int level,
+            Dictionary<Tuple<int, int>, int> sums)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            var numberOfPaths = 0;
+            if (node.Data == target)
+            {
+                numberOfPaths++;
+            }
+
+            sums[new Tuple<int, int>(level, level)] = node.Data;
+            for (int i = 0; i < level; i++)
+            {
+                sums[new Tuple<int, int>(i, level)] = sums[new Tuple<int, int>(i, level - 1)] + node.Data;
+                if (sums[new Tuple<int, int>(i, level)] == target)
+                {
+                    numberOfPaths++;
+                }
+            }
+
+            numberOfPaths += SumPathsHelper(node.Left, target, level + 1, sums);
+            numberOfPaths += SumPathsHelper(node.Right, target, level + 1, sums);
+            return numberOfPaths;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 4/Test_4_12.cs
@@ -0,0 +1,69 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_4
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_4;
+    using Questions.Data_structures;
+
+    [TestClass]
+    public class Test_4_12
+    {
+        [TestMethod]
+        public void Question_4_12_BasicCases()
+        {
+            // Empty tree.
+            BinaryTreeNode<int> tree = null;
+            Validate(0, tree, 2);
+            Validate(0, tree, 0);
+
+            // Single Node tree tests.
+            tree = TreeHelpers.CreateBinaryTree(0, null, null);
+            Validate(0, tree, 1);
+            Validate(1, tree, 0);
+
+            // Small tree;
+            tree = TreeHelpers.CreateBinaryTree(1, 0, 2);
+            Validate(2, tree, 1);
+            Validate(1, tree, 0);
+            Validate(1, tree, 2);
+            Validate(1, tree, 3);
+            Validate(0, tree, 4);
+            Validate(0, tree, -1);
+
+            // Awkward lopsided tree.
+            //   0
+            //    \
+            //     1
+            //      \
+            //       2
+            //        ...
+            tree = TreeHelpers.CreateBinaryTreeFromArray(0, 1, 2, 3, 4, 5);
+            Validate(2, tree, 1);
+            Validate(2, tree, 5);
+            Validate(2, tree, 15);
+            Validate(3, tree, 3);
+            Validate(1, tree, 4);
+            Validate(0, tree, 8);
+
+            // Bigger tree
+            //                3
+            //              /   \
+            //             1     5
+            //            / \   / \
+            //           2   0 4   6
+            tree = TreeHelpers.CreateBinaryTreeFromArray<int>(3, 1, 0, 2, 5, 4, 6);
+            Validate(1, tree, 5);
+            Validate(2, tree, 6);
+            Validate(1, tree, 12);
+            Validate(3, tree, 4);
+            Validate(1, tree, 14);
+            Validate(2, tree, 1);
+            Validate(1, tree, 2);
+        }
+
+        private static void Validate(int expectedPathCount, BinaryTreeNode<int> root, int target)
+        {
+            var result = Question_4_12.SumPaths(root, target);
+            Assert.AreEqual(expectedPathCount, result);
+        }
+    }
+}
