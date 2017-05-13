2017c51722a38c91489144a2232cf43a6190c4a8
John Krzemien <john.krzemien@gmail.com>
2017-05-13 11:59:40 -0700

    Adding question 5.2 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 5/Question_5_2.cs
@@ -0,0 +1,45 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_5
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// Binary to String: Given a real number between 8 and 1 (e.g., 0.72) that is passed in as a double,
+    /// print the binary representation.If the number cannot be represented accurately in binary with at
+    /// most 32 characters, print "ERROR:' 
+    /// </summary>
+    public class Question_5_2
+    {
+        public static string BinaryDecimalToString(double number)
+        {
+            if (number >= 1 || number < 0)
+            {
+                throw new ArgumentException(nameof(number));
+            }
+
+            var sb = new StringBuilder();
+            sb.Append(".");
+            while (number > 0)
+            {
+                if (sb.Length > 32)
+                {
+                    throw new InvalidOperationException("Number is too long.");
+                }
+
+                var next = number * 2; // Shift over bits
+                if (next >= 1)
+                {
+                    sb.Append("1");
+                    number = next - 1;
+                }
+                else
+                {
+                    sb.Append("0");
+                    number = next;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 5/Test_5_2.cs
@@ -0,0 +1,31 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_5
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_5;
+
+    [TestClass]
+    public class Test_5_2
+    {
+        [TestMethod]
+        public void Question_5_2_BasicCases()
+        {
+            double number = 0.625;
+            string expected = ".101";
+            Validate(expected, number);
+
+            number = 0.375;
+            expected = ".011";
+            Validate(expected, number);
+
+            number = 0.5;
+            expected = ".1";
+            Validate(expected, number);
+        }
+
+        private static void Validate(string expected, double number)
+        {
+            var result = Question_5_2.BinaryDecimalToString(number);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
