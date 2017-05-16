135ee29a387ce1257f0abd7978a91054f759cc60
John Krzemien <john.krzemien@gmail.com>
2017-05-15 20:31:26 -0700

    Add question 5.6 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 5/Question_5_6.cs
@@ -0,0 +1,28 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_5
+{
+    /// <summary>
+    ///  Conversion: Write a function to determine the number of bits you would need to flip to convert
+    /// integer A to integer B.
+    /// </summary>
+    public class Question_5_6
+    {
+        // Time: O(n) where n is number of bits
+        // Space: O(1)
+        public static int ConversionCount(int a, int b)
+        {
+            var diff = a ^ b;
+            var count = 0;
+            while (diff != 0)
+            {
+                if ((diff & 1) != 0)
+                {
+                    count++;
+                }
+
+                diff >>= 1;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 5/Test_5_6.cs
@@ -0,0 +1,46 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_5
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_5;
+
+    [TestClass]
+    public class Test_5_6
+    {
+        [TestMethod]
+        public void Question_5_6_BasicCases()
+        {
+            int expected = 2;
+            int a = 29;
+            int b = 15;
+            Validate(expected, a, b);
+
+            a = 0;
+            expected = 4;
+            Validate(expected, a, b);
+
+            a = 1;
+            expected = 3;
+            Validate(expected, a, b);
+
+            a = 3;
+            expected = 2;
+            Validate(expected, a, b);
+
+            a = 4;
+            expected = 3;
+            Validate(expected, a, b);
+
+            a = 16;
+            expected = 5;
+            Validate(expected, a, b);
+        }
+
+        private static void Validate(int expected, int a, int b)
+        {
+            var result = Question_5_6.ConversionCount(a, b);
+            Assert.AreEqual(expected, result);
+            result = Question_5_6.ConversionCount(b, a);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
