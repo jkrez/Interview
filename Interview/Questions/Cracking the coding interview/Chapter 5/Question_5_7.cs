b169d67284dfdcd9e085e64a0583b4c50a7f03f9
John Krzemien <john.krzemien@gmail.com>
2017-05-15 21:57:58 -0700

    Adding question 5.7 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 5/Question_5_7.cs
@@ -0,0 +1,17 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_5
+{
+    /// <summary>
+    /// Pairwise Swap: Write a program to swap odd and even bits in an integer with as few instructions as
+    /// possible(e.g., bit 0 and bit 1 are swapped, bit 2 and bit 3 are swapped, and so on). 
+    /// </summary>
+    public class Question_5_7
+    {
+        private const int oddMask = 0x55555555;
+        private const int evenMask = oddMask << 1;
+
+        public static int SwapBits(int n)
+        {
+            return ((n & evenMask) >> 1) | ((n & oddMask) << 1);
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 5/Test_5_7.cs
@@ -0,0 +1,40 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_5
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_5;
+
+    [TestClass]
+    public class Test_5_7
+    {
+        [TestMethod]
+        public void Question_5_7_BasicCases()
+        {
+            var n = 1;
+            var expected = 2;
+            Validate(expected, n);
+
+            n = 2;
+            expected = 1;
+            Validate(expected, n);
+
+            n = 3;
+            expected = 3;
+            Validate(expected, n);
+
+            n = 4;
+            expected = 8;
+            Validate(expected, n);
+
+            n = 5;
+            expected = 10;
+            Validate(expected, n);
+
+        }
+
+        private void Validate(int expected, int n)
+        {
+            var result = Question_5_7.SwapBits(n);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
