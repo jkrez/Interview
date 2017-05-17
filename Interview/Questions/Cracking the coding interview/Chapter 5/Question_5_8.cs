dca763196ec473c3d1165acf8e954a3d2dffba71
John Krzemien <john.krzemien@gmail.com>
2017-05-16 20:28:16 -0700

    Adding question 5.8 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 5/Question_5_8.cs
@@ -0,0 +1,51 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_5
+{
+    using System;
+
+    /// <summary>
+    /// Draw Line: A monochrome screen is stored as a single array of bytes, allowing eight consecutive
+    /// pixels to be stored in one byte. The screen has width w, where w is divisible by 8 (that is, no byte will
+    /// be split across rows). The height of the screen, of course, can be derived from the length of the array
+    /// and the width. Implement a function that draws a horizontal line from (xl, y) to (x2, y).
+    /// </summary>
+    public class Question_5_8
+    {
+        private const int byteSize = 8;
+
+        public static void DrawScreen(byte[] screen, int width, int x1, int x2, int y)
+        {
+            if (screen == null)
+            {
+                throw new ArgumentNullException(nameof(screen));
+            }
+
+            if (width < sizeof(byte))
+            {
+                throw new ArgumentOutOfRangeException(nameof(width));
+            }
+
+            if ((x1 < 0 || x2 < 0) ||
+                (x1 >= width || x2 >= width) ||
+                (x2 < x1))
+            {
+                throw new ArgumentOutOfRangeException("x1 and x2");
+            }
+
+            int start = y * width + x1;
+            int end = y * width + x2;
+            byte ones = 0xFF;
+            int b = ones >> (x1 % byteSize);
+            screen[start / byteSize] = (byte)b;
+            int current = start + (byteSize - (start % byteSize));
+            while (current < end)
+            {
+                screen[current / byteSize] = 0xFF;
+                current += byteSize;
+            }
+
+            int bitsToRemove = (byteSize - 1) - (end % byteSize);
+            b = -1 << bitsToRemove;
+            screen[end / byteSize] &= (byte)b;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 5/Test_5_8.cs
@@ -0,0 +1,65 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_5
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_5;
+
+    [TestClass]
+    public class Test_5_8
+    {
+        [TestMethod]
+        public void Question_5_8_BasicCases()
+        {
+            // One bit line tests.
+            var height = 8;
+            var width = 24;
+            var screen = new byte[width/8 * height];
+            screen[2 * width/8] = 0x18;
+            Validate(screen, width, 3, 4, 2);
+
+            screen = new byte[width / 8 * height];
+            screen[(2 * width / 8) + 1] = 0xC0;
+            Validate(screen, width, 8, 9, 2);
+
+            screen = new byte[width / 8 * height];
+            screen[(7 * width / 8) + 2] = 0x01;
+            Validate(screen, width, 23, 23, 7);
+
+            // Multi bit line tests
+            screen = new byte[width / 8 * height];
+            screen[(7 * width / 8) + 2] = 0xFF;
+            Validate(screen, width, 16, 23, 7);
+
+            screen = new byte[width / 8 * height];
+            screen[(7 * width / 8)] = 0xFF;
+            Validate(screen, width, 0, 7, 7);
+
+            screen = new byte[width / 8 * height];
+            screen[0] = 0xFF;
+            screen[1] = 0xFF;
+            screen[2] = 0xFF;
+            Validate(screen, width, 0, 23, 0);
+
+            screen = new byte[width / 8 * height];
+            screen[0] = 0xFF;
+            screen[1] = 0xF0;
+            Validate(screen, width, 0, 11, 0);
+        }
+
+        [TestMethod]
+        public void Question_5_8_InvalidCases()
+        {
+            TestHelpers.AssertExceptionThrown(() => Question_5_8.DrawScreen(null, 8, 1, 2, 3), typeof(ArgumentNullException));
+            TestHelpers.AssertExceptionThrown(() => Question_5_8.DrawScreen(new byte[100], 1, 1, 2, 3), typeof(ArgumentOutOfRangeException));
+            TestHelpers.AssertExceptionThrown(() => Question_5_8.DrawScreen(new byte[100], 8, -1, 2, 3), typeof(ArgumentOutOfRangeException));
+            TestHelpers.AssertExceptionThrown(() => Question_5_8.DrawScreen(new byte[100], 8, 1000, 2, 3), typeof(ArgumentOutOfRangeException));
+        }
+
+        private void Validate(byte[] expected, int width, int x1, int x2, int y)
+        {
+            byte[] screen = new byte[expected.Length];
+            Question_5_8.DrawScreen(screen, width, x1, x2, y);
+            CollectionAssert.AreEqual(expected, screen);
+        }
+    }
+}
