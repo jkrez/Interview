617b8084308f2c277f85d74d57f77f9405fb5613
John Krzemien <john.krzemien@gmail.com>
2017-05-17 20:12:22 -0700

    Adding question 8.1 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 8/Question_8_1.cs
@@ -0,0 +1,46 @@
+﻿namespace Questions.Cracking_the_coding_interview
+{
+    using System;
+
+    /// <summary>
+    /// Triple Step: A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3
+    ///  steps at a time.Implement a method to count how many possible ways the child can run up the
+    /// stairs.
+    /// </summary>
+    public class Question_8_1
+    {
+        // Time: O(n)
+        // Space: O(1)
+        public static int NumberOfWays(int numberOfSteps)
+        {
+            if (numberOfSteps < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberOfSteps));
+            }
+
+            switch (numberOfSteps)
+            {
+                case 0:
+                    return 1;
+                case 1:
+                    return 2;
+                case 2:
+                    return 4;
+            }
+
+            int n;
+            int nm1 = n = 4;
+            int nm2 = 2;
+            int nm3 = 1;
+            for (int i = 3; i <= numberOfSteps; i++)
+            {
+                n = nm1 + nm2 + nm3;
+                nm3 = nm2;
+                nm2 = nm1;
+                nm1 = n;
+            }
+
+            return n;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 8/Test_8_1.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests.Cracking_the_coding_interview
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview;
+
+    [TestClass]
+    public class Test_8_1
+    {
+        [TestMethod]
+        public void Question_8_1_BasicCases()
+        {
+            Validate(1, 0);
+            Validate(2, 1);
+            Validate(4, 2);
+            Validate(7, 3);
+            Validate(13, 4);
+            Validate(24, 5);
+
+        }
+
+        [TestMethod]
+        public void Question_8_1_InvalidCases()
+        {
+            TestHelpers.AssertExceptionThrown(() => Question_8_1.NumberOfWays(-1), typeof(ArgumentOutOfRangeException));
+        }
+
+        private static void Validate(int expected, int numberOfSteps)
+        {
+            var result = Question_8_1.NumberOfWays(numberOfSteps);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
