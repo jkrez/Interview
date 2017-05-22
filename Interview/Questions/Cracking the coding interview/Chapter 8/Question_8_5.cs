61afd94e0ed517d2539c8e67bd790d59a7d0f70d
John Krzemien <john.krzemien@gmail.com>
2017-05-21 19:26:13 -0700

    Adding question 8.5 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 8/Question_8_5.cs
@@ -0,0 +1,45 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_8
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Recursive Multiply: Write a recursive function to multiply two positive integers without using the
+    /// * operator. You can use addition, subtraction, and bit shifting, but you should minimize the number
+    /// of those operations.
+    /// </summary>
+    public class Question_8_5
+    {
+        // Time: O(log(smaller))
+        // Space: O (log(smaller))
+        public static int RecursiveMultiply(int a, int b)
+        {
+            int bigger = a > b ? a : b;
+            int smaller = a > b ? b : a;
+            return RecursiveMultipleHelper(smaller, bigger);
+        }
+
+        private static int RecursiveMultipleHelper(int smaller, int bigger)
+        {
+            if (smaller == 0)
+            {
+                return 0;
+            }
+
+            if (smaller == 1)
+            {
+                return bigger;
+            }
+
+            var halfSmaller = smaller >> 1;
+            int half = RecursiveMultipleHelper(halfSmaller, bigger);
+            if (smaller % 2 == 0)
+            {
+                return half + half;
+            }
+            else
+            {
+                return half + half + bigger;
+            }
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 8/Test_8_5.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_8
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_8;
+
+    [TestClass]
+    public class Test_8_5
+    {
+        [TestMethod]
+        public void Question_8_5_BasicCases()
+        {
+            foreach (var a in Enumerable.Range(0, 50))
+            {
+                foreach (var b in Enumerable.Range(0, 50))
+                {
+                    Validate(a, b);
+                }
+            }
+        }
+
+        private static void Validate(int a, int b)
+        {
+            var expected = a * b;
+            var result = Question_8_5.RecursiveMultiply(a, b);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
