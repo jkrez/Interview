c9a0944bc91b1d3b6a85f4303b573df2fc530fe9
John Krzemien <john.krzemien@gmail.com>
2017-05-24 08:22:19 -0700

    Adding question 8.6 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 8/Question_8_6.cs
@@ -0,0 +1,58 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_8
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Towers of Hanoi: In the classic problem of the Towers of Hanoi, you have 3 towers and N disks of
+    /// different sizes which can slide onto any tower.The puzzle starts with disks sorted in ascending order
+    /// of size from top to bottom (Le., each disk sits on top of an even larger one). You have the following
+    /// constraints:
+    /// (1) Only one disk can be moved at a time.
+    /// (2) A disk is slid off the top of one tower onto another tower.
+    /// (3) A disk cannot be placed on top of a smaller disk.
+    /// Write a program to move the disks from the first tower to the last using stacks.
+    /// </summary>
+    public class Question_8_6
+    {
+        public static void MoveTower(Stack<int> src, Stack<int> dest, Stack<int> temp)
+        {
+            if (src.Count == 0)
+            {
+                return;
+            }
+
+            MoveTowerCount(src, dest, temp, src.Count);
+        }
+
+        private static void MoveTowerCount(Stack<int> src, Stack<int> dest, Stack<int> temp, int count)
+        {
+            if (count == 1)
+            {
+                Move(src, dest);
+            }
+            else if (count == 2)
+            {
+                Move(src, temp);
+                Move(src, dest);
+                Move(temp, dest);
+            }
+            else
+            {
+                MoveTowerCount(src, temp, dest, count - 1);
+                Move(src, dest);
+                MoveTowerCount(temp, dest, src, count - 1);
+            }
+        }
+
+        private static bool CanMoveSingleDisk(Stack<int> src, Stack<int> dest)
+        {
+            return dest.Count == 0 || src.Peek() < dest.Peek();
+        }
+
+        private static void Move(Stack<int> src, Stack<int> dest)
+        {
+            dest.Push(src.Peek());
+            src.Pop();
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 8/Test_8_6.cs
@@ -0,0 +1,51 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_8
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_8;
+
+    [TestClass]
+    public class Test_8_6
+    {
+        [TestMethod]
+        public void Question_8_6_BasicCases()
+        {
+            for (int stackHeight = 1; stackHeight < 10; stackHeight++)
+            {
+                var src = CreateStack(stackHeight);
+                var dest = CreateStack(0);
+                var temp = CreateStack(0);
+                Validate(src, dest, temp);
+            }
+        }
+
+        private static void Validate(Stack<int> src, Stack<int> dest, Stack<int> temp)
+        {
+            var size = src.Count;
+            Question_8_6.MoveTower(src, dest, temp);
+            Assert.AreEqual(0, src.Count);
+            Assert.AreEqual(0, temp.Count);
+            Assert.AreEqual(size, dest.Count);
+            var destCopy = new Stack<int>(dest);
+            var last = -1;
+            while (destCopy.Count != 0)
+            {
+                var next = destCopy.Pop();
+                Assert.IsTrue(last < next);
+            }
+        }
+
+        private static Stack<int> CreateStack(int size)
+        {
+            var s = new Stack<int>();
+            for (int i = 0; i < size; i++)
+            {
+                s.Push(size - i);
+            }
+
+            return s;
+        }
+    }
+}
