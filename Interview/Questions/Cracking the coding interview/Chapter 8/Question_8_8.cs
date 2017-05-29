dda104a042813d56fd6914cea08efb5b418e9674
John Krzemien <john.krzemien@gmail.com>
2017-05-29 13:02:15 -0700

    Adding question 8.8 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 8/Question_8_8.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions.Cracking_the_coding_interview.Chapter_8
+{
+    public class Question_8_8
+    {
+        public static List<string> PermutationWithDuplicates(string input)
+        {
+            var set = new HashSet<char>();
+            foreach (var c in input)
+            {
+                set.Add(c);
+            }
+
+            var list = set.ToList();
+            var str = new string(list.ToArray());
+            return Question_8_7.PermutationsWithoutDuplicates(str);
+        }
+
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 8/Test_8_8.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_8
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_8;
+
+    [TestClass]
+    public class Test_8_8
+    {
+        [TestMethod]
+        public void Question_8_8_BasicCases()
+        {
+            var s = "";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+            s = "a";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+            s = "abc";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+            s = "aa";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+            s = "aaaa";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+            s = "aaabababaaabbbbb";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+            s = "abcabcabc";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+            s = "abcdabcdbababadcc";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+            s = "abcd";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+            s = "abcdef";
+            Test_8_7.Validate(s, Question_8_8.PermutationWithDuplicates);
+        }
+    }
+}
