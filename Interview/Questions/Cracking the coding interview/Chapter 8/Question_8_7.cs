eba2378f4d847aad4be45071b61f0b597d3eb1c3
John Krzemien <john.krzemien@gmail.com>
2017-05-29 12:48:46 -0700

    Adding question 8.7 solution

---
+++ Interview/Questions/Cracking the coding interview/Chapter 8/Question_8_7.cs
@@ -0,0 +1,50 @@
+﻿namespace Questions.Cracking_the_coding_interview.Chapter_8
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// Permutations without Dups: Write a method to compute all permutations of a string of unique
+    /// characters.
+    /// </summary>
+    public class Question_8_7
+    {
+        // Time: O(n^2 * n!)
+        // Space: O(n^2 * n!)
+        public static List<string> PermutationsWithoutDuplicates(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return new List<string>();
+            }
+
+            var set = new HashSet<char>();
+            foreach (var c in input)
+            {
+                set.Add(c);
+            }
+
+            var result = new List<string>();
+            var list = set.ToList();
+            result.Add(string.Empty + list[0]);
+            for (int i = 1; i < list.Count; i++)
+            {
+                var temp = new List<string>();
+                foreach (var str in result)
+                {
+                    for (int j = 0; j <= str.Length; j++)
+                    {
+                        temp.Add(str.Substring(0,j) +
+                                 list[i] +
+                                 str.Substring(j));
+                    }
+                }
+
+                result = temp;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Interview/Tests/Cracking the coding interview/Chapter 8/Test_8_7.cs
@@ -0,0 +1,83 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_8
+{
+    using System.CodeDom;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_8;
+
+    [TestClass]
+    public class Test_8_7
+    {
+        [TestMethod]
+        public void Question_8_7_BasicCases()
+        {
+            var s = "";
+            Validate(s);
+            s = "a";
+            Validate(s);
+            s = "abc";
+            Validate(s);
+            s = "aa";
+            Validate(s);
+            s = "aaaa";
+            Validate(s);
+            s = "aaabababaaabbbbb";
+            Validate(s);
+            s = "abcabcabc";
+            Validate(s);
+            s = "abcdabcdbababadcc";
+            Validate(s);
+            s = "abcd";
+            Validate(s);
+            s = "abcdef";
+            Validate(s);
+        }
+
+        private void Validate(string input)
+        {
+            var permutations = new HashSet<string>();
+            var result = Question_8_7.PermutationsWithoutDuplicates(input);
+            var unique = RemoveDuplicates(input);
+            var expectedCount = CountPerm(unique.Length);
+            Assert.AreEqual(expectedCount, result.Count);
+            foreach (var perm in result)
+            {
+                Assert.IsFalse(permutations.Contains(perm));
+                permutations.Add(perm);
+            }
+        }
+
+        private static string RemoveDuplicates(string s)
+        {
+            var result = "";
+            var seen = new HashSet<char>();
+            foreach (var c in s)
+            {
+                if (seen.Contains(c))
+                {
+                    continue;
+                }
+
+                seen.Add(c);
+                result += c;
+            }
+
+            return result;
+        }
+
+        private static int CountPerm(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return CountPerm(n - 1) * n;
+        }
+    }
+}
