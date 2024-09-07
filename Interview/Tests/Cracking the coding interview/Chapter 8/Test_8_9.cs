3297679c6a7d913aaf74284fdac93bd3144c52f9
John Krzemien <916327+jkrez@users.noreply.github.com>
2024-09-07 15:29:33 -0700

    Adding question 8.9 solution

---
+++ Interview/Tests/Cracking the coding interview/Chapter 8/Test_8_9.cs
@@ -0,0 +1,31 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_8
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_8;
+
+    [TestClass]
+    public class Test_8_9
+    {
+        [TestMethod]
+        public void Question_8_9_BasicCases()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Validate(i);
+            }
+        }
+
+        private static void Validate(int n)
+        {
+            var result = Question_8_9.GetParenthesis(n);
+            var set = new HashSet<string>();
+            foreach (var str in result)
+            {
+                set.Add(str);
+            }
+
+            Assert.AreEqual(set.Count, result.Count);
+        }
+    }
+}
