3facacbf214e981645f6d4d7b94cb0d0c5b63be8
John Krzemien <916327+jkrez@users.noreply.github.com>
2024-09-07 15:29:33 -0700

    Adding robot path counting question and moving extra question folder

---
+++ Interview/Tests/Extra Questions/RobotPathsCount.cs
@@ -0,0 +1,37 @@
+﻿namespace Tests.Extra_Questions
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Qualtrics;
+
+    [TestClass]
+    public class RobotPathsCount
+    {
+        [TestMethod]
+        public void Extra_RobotCountPaths()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Validate(i,j);
+                }
+            }
+        }
+
+        private static void Validate(int m, int n)
+        {
+            var expected = Factorial(m - 1 + n - 1) / (Factorial(m - 1) * Factorial(n - 1));
+            var actual = RobotProblem.CountPaths(m, n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static Int64 Factorial(int n)
+        {
+            if (n == 1 || n == 0)
+                return 1;
+
+            return Factorial(n - 1) * n;
+        }
+    } 
+}
