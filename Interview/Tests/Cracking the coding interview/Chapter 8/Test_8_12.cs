db6e7d1dde57167cdcecabcd3c16876390fbd557
John Krzemien <916327+jkrez@users.noreply.github.com>
2024-09-07 15:29:33 -0700

    Adding question 8.12 solution

---
+++ Interview/Tests/Cracking the coding interview/Chapter 8/Test_8_12.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests.Cracking_the_coding_interview.Chapter_8
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Questions.Cracking_the_coding_interview.Chapter_8;
+
+    [TestClass]
+    public class Test_8_12
+    {
+        [TestMethod]
+        public void Question_8_12_BasicCases()
+        {
+            var boardSize = 8;
+            var expected = 92;
+            var result = Question_8_12.PlaceQueensWays(boardSize, 0, new bool[boardSize, boardSize], new bool[boardSize],
+                new bool[boardSize*2], new bool[boardSize*2]);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
