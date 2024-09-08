55c0b30aeb843ff5e1a516691824b83bb40d8866
John Krzemien <916327+jkrez@users.noreply.github.com>
2024-09-07 20:26:43 -0700

    faster

--- Interviews2024/Leetcode75/problems/arrays/product_array.py
+++ Interviews2024/Leetcode75/problems/arrays/product_array.py
@@ -3,17 +3,27 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        lprod: List[int] = [1] * (len(nums))
-        rprod: List[int] = [1] * (len(nums))
-        for idx in range(1, len(nums), 1):
-            lprod[idx] = lprod[idx - 1] * nums[idx - 1]
+        # lprod: List[int] = [1] * (len(nums))
+        # rprod: List[int] = [1] * (len(nums))
+        # for idx in range(1, len(nums), 1):
+        #     lprod[idx] = lprod[idx - 1] * nums[idx - 1]
 
         # range() fn takes: (start (inclusive), stop (exclusive), step)
-        for idx in range(len(nums) - 2, -1, -1):
-            rprod[idx] = rprod[idx + 1] * nums[idx + 1]
+        # for idx in range(len(nums) - 2, -1, -1):
+        #     rprod[idx] = rprod[idx + 1] * nums[idx + 1]
+        #
+        # res: List[int] = [0] * len(nums)
+        # for idx in range(len(nums)):
+        #     res[idx] = lprod[idx] * rprod[idx]
 
-        res: List[int] = [0] * len(nums)
-        for idx in range(len(nums)):
-            res[idx] = lprod[idx] * rprod[idx]
+        # less space
+        res: List[int] = [1] * len(nums)
+        for idx in range(1, len(nums)):
+            res[idx] = res[idx - 1] * nums[idx - 1]
+
+        r_product: int = 1
+        for idx in reversed(range(len(nums))):
+            res[idx] = res[idx] * r_product
+            r_product *= nums[idx]
 
         return res
